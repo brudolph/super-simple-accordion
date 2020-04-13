549922053f2cc1990db82643a55cc6df5caf35d6
brudolph <>
2020-04-12 21:24:23 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,149 @@
+Super simple, accessible, content accordion.
+
+## Install
+
+1. Add Super Simple Accordion
+
+Available on npm:
+
+```sh
+$ npm install super-simple-accordion
+
+```
+
+Or download the repo and just link to it old school. You can use the minified version in the dist folder.
+
+```html
+<script src="/path/to/js/accordion.min.js"></script>
+```
+
+## Usage
+
+## Step 1. Include CSS
+
+```html
+<link rel="stylesheet" href="/dist/css/main.css" />
+```
+
+## Step 2. Include markup
+
+In your project include the following markup.
+
+```html
+<section>
+<h1>Section Title for these grouped accordions</h1>
+
+  <article id="faq1" class="accordion">
+    <h2 class="accordion__title">
+      <span class="accordion__text">What is an accordion?</span>
+    </h2>
+    <div class="accordion__panel">
+      <div class="accordion__content">
+        <p>
+          Accordions are useful when you want to toggle between hiding and showing large amounts of content.
+        </p>
+        <p>
+          Look at all of this beautiful content!
+        </p>
+        <p>
+          It all seems so gratuitous but ohhhhh so needed. I love hearing myself talk (type?)!
+        </p>
+      </div>
+    </div>
+  </article>
+
+  <article id="faq2" class="accordion">
+    <h2 class="accordion__title">
+      <span class="accordion__text">Who is involved?</span>
+    </h2>
+    <div class="accordion__panel">
+      <div class="accordion__content">
+        <p>Accordions are useful when you want to toggle between hiding and showing large amounts of content.</p>
+      </div>
+    </div>
+  </article>
+   
+ </section>
+```
+This is just some example markup and can be changed to fit your needs. You will just need these five elements:
+
+ 1. A container (does not need to be an article) for your accordion with the class `.accordion`. 
+ 2. A header with the class `.accordion__title`.
+ 3. An element inside of the header with the class `.accordion__text`. This is used to create the button
+ 4. A container for the content with the class `.accordion__panel`
+ 5. The content with a class of `.accordion__content`
+
+## Step 3. Include JavaScript
+If you would like to use as an ES6 module jump to [ES6 Modules](#es6-modules) below and skip this step.
+
+If you are using the minified version and linking to it, include the accordion script and initialize it.
+
+```html
+<script src="/dist/js/accordions.min.js"></script>
+<script>
+ const accordion = new SuperSimpleAccordions('.accordion');
+</script>
+```
+## ES6 Modules
+
+```sh
+$ npm install
+
+```
+
+Open up main.js in the src directory and customize the options to your liking. Below are the default options available. Only include the ones you plan to change.
+
+main.js
+
+```js
+import SuperSimpleAccordions from './accordions';
+
+if (document.querySelector(".accordion")) {
+    const accordions = new SuperSimpleAccordions('.accordion', {
+      // ALL available options
+
+      // Accordion parts
+       accordionClass: 'accordion',
+       headerClass: 'accordion__title',
+       contentClass: 'accordion__content',
+       panelClass: 'accordion__panel',
+
+       // Toggle Button
+       toggleBtnClass: 'accordion__toggle',
+
+       // Icons
+       icons: true, // true or false
+       iconsClass: 'accordion__icon',
+       iconsSymbol: 'plus-minus', // arrow or plus-minus
+       iconsPosition: 'left', // right or left
+
+       // Expand All features
+       expandAllBtn: true, // true or false 
+       expandAllClass: 'accordion__toggle-all',
+       expandAllContainerClass: 'accordion__toggle-all-container',
+       expandSelectClass: 'expanded',
+       expandAllOpenText: 'Expand All',
+       expandAllCloseText: 'Collapse All',
+       expanded: false, // accordions collapsed by default but an be expanded
+    })
+  }
+```
+
+If you do change the class names makes sure to update your SASS files accordingly.
+
+
+```sh
+$ npm run prod
+
+```
+
+This will create a file named accordions.js in the dist directory.
+
+```html
+<script src="/dist/js/accordions.js"></script>
+```
+
+## TODO
+
+Refactor and make into ES6 modules maybe a React component?
+
---
+++ src/js/accordions.js
@@ -0,0 +1,389 @@
+(function(root, factory) {
+  // eslint-disable-next-line no-undef
+  if (typeof define === 'function' && define.amd) {
+    define([], function() {
+      return factory(root);
+    });
+  } else if (typeof exports === 'object') {
+    module.exports = factory(root);
+  } else {
+    root.SuperSimpleAccordions = factory(root);
+  }
+})(
+  // eslint-disable-next-line no-nested-ternary
+  typeof global !== 'undefined'
+    ? global
+    : typeof window !== 'undefined'
+    ? window
+    : this,
+  function(window) {
+    // Use strict mode
+
+    // Create an empty plugin object
+    const plugin = {};
+
+    // Set the plugin defaults
+    const defaults = {
+      // Accordion parts
+      accordionClass: 'accordion',
+      headerClass: 'accordion__title',
+      contentClass: 'accordion__content',
+      panelClass: 'accordion__panel',
+
+      // Toggle Button
+      toggleBtnClass: 'accordion__toggle',
+
+      // Icons
+      icons: true,
+      iconsSymbol: 'plus-minus', // arrow
+      iconsPosition: 'left', // right
+
+      // Expand All features
+      expandAllBtn: true,
+      expandAllClass: 'accordion__toggle-all',
+      expandAllContainerClass: 'accordion__toggle-all-container',
+      expandSelectClass: 'expanded',
+      expandAllOpenText: 'Expand All',
+      expandAllCloseText: 'Collapse All',
+      expanded: false, // expanded or collapsed by default
+    };
+
+    /**
+     * Constructor.
+     * @param  {String}  element  The selector element(s).
+     * @param  {Object}   options  The plugin options.
+     * @return {void}
+     */
+    function Plugin(element, options) {
+      // Set the plugin object
+      plugin.this = this;
+      plugin.element = element;
+      plugin.defaults = defaults;
+      plugin.options = options;
+      // Merge user settings with defaults
+      plugin.settings = Object.assign({}, defaults, options);
+
+      // Initialize the plugin
+      plugin.this.init();
+    }
+
+    //
+    // Methods
+    //
+
+    /**
+     * If icon is true then create and setup icon
+     * 
+     * @function accordionIconSetup
+     * @param  {String} toggle The toggle button
+     * @return {void} 
+     */
+    const accordionIconSetup = function(toggle) {
+      const toggleBtn = toggle;
+      if (plugin.settings.icons === true) {
+        const iconContainer = document.createElement('span');
+        iconContainer.classList.add(plugin.settings.iconsSymbol);
+        iconContainer.classList.add(plugin.settings.iconsPosition);
+        toggle.appendChild(iconContainer);
+      } else {
+        toggleBtn.classList.add('icon-hidden');
+      }
+    };
+
+    /**
+     * Setup plugin.settings.panelClass which contains the content
+     *
+     * @function accordionContentSetup
+     * @param  {String} content The accordion container.
+     * @param  {String} id The accordion id.
+     * @return {void}
+     */
+    const accordionContentSetup = function(content, id) {
+      // Get accordion content from plugin.settings.accordionClass container
+      const accordionPanel = content.querySelector(
+        `.${plugin.settings.panelClass}`
+      );
+      // Create div for accordion content add content class
+      const accordionContent = document.createElement('div');
+      accordionContent.className = plugin.settings.contentClass;
+      // Add content from panel
+      accordionContent.innerHTML = accordionPanel.innerHTML;
+
+      // Clear accordion panel html since it was added to content div
+      accordionPanel.innerHTML = '';
+      // Append content div to accordion panel
+      accordionPanel.appendChild(accordionContent);
+
+      accordionPanel.setAttribute('id', `accordion${id}`);
+
+      // Check to see if the accordions are set to be expanded or collapsed
+      if (plugin.settings.expanded === false) {
+        accordionPanel.setAttribute('aria-hidden', 'true');
+        accordionPanel.style.height = '0px';
+      } else {
+        accordionPanel.setAttribute('aria-hidden', 'false');
+        accordionPanel.style.height = `${accordionPanel.scrollHeight}px`;
+      }
+    };
+
+    /**
+     * Setup plugin.settings.toggleBtnClass, the individual accordion toggle button
+     *
+     * @function accordionToggleSetup
+     * @param  {String} content The accordion button.
+     * @param  {String} btnId The accordion button id.
+     *
+     */
+    const accordionToggleSetup = function(content, btnId) {
+      // Get accordion header from plugin.settings.accordionClass
+      const accordionHeader = content.querySelector(
+        `.${plugin.settings.headerClass}`
+      );
+
+      // Create a toggle button, add toggle class and aria attributes
+      const toggle = document.createElement('button');
+      toggle.className = plugin.settings.toggleBtnClass;
+      toggle.setAttribute('aria-controls', `accordion${btnId}`);
+      // Check to see if the accordions are set to be expanded or collapsed
+      if (plugin.settings.expanded === false) {
+        toggle.setAttribute('aria-expanded', 'false');
+      } else {
+        toggle.setAttribute('aria-expanded', 'true');
+      }
+      
+
+      // Get content from existing header and add to button
+      toggle.innerHTML = accordionHeader.innerHTML;
+
+      // Clear accordion header html since it was added to button
+      accordionHeader.innerHTML = '';
+      // Append button to accordion header
+      accordionHeader.appendChild(toggle);
+
+      // Initialize the accordion content and icon
+      accordionContentSetup(content, btnId);
+      accordionIconSetup(toggle);
+    };
+
+    /**
+     * Create and setup the toggle all button
+     *
+     * @function toggleAllSetup
+     * @return {void}
+     */
+    const toggleAllSetup = function() {
+      // find first accodrion if only one the return
+      const firstAccordion = document.querySelector(
+        `.${plugin.settings.accordionClass}`
+      );
+
+      if (!firstAccordion) return;
+
+      // create button container
+      const buttonContainer = document.createElement('div');
+      buttonContainer.className = plugin.settings.expandAllContainerClass;
+
+      // create expand all button
+      const button = document.createElement('button');
+      button.className = plugin.settings.expandAllClass;
+      // Check to see if the accordions are set to be expanded or collapsed
+      if (plugin.settings.expanded === false) {
+        button.setAttribute('aria-expanded', 'false');
+        button.textContent = plugin.settings.expandAllOpenText;
+      } else {
+        button.setAttribute('aria-expanded', 'true');
+        button.textContent = plugin.settings.expandAllCloseText;
+      }
+
+      // insert button into container
+      buttonContainer.appendChild(button);
+
+      // find parent element of first accordion
+      const parentContainer = firstAccordion.parentNode;
+
+      // insert button container before first accordion
+      parentContainer.insertBefore(buttonContainer, firstAccordion);
+    };
+
+    /**
+     * 
+     * 
+     * @function toggleButton
+     * @param  {String} targetElem  The toggle button
+     * @param  {String} direction   The direction of the accordion Expand/Collapse
+     * @return {void}
+     */
+    const toggleButton = function(targetElem, direction) {
+      if (direction === 'expand') {
+        targetElem.setAttribute('aria-expanded', 'true');
+      } else {
+        targetElem.setAttribute('aria-expanded', 'false');
+      }
+    };
+
+    /**
+     * @function toggleContent
+     * @param  {String} targetElem The Accordion button
+     * @param  {MouseEvent} event mouse or keyboard
+     * @param  {String} direction The direction of the accordion Expand/Collapse
+     * @return {void}
+     */
+    const toggleAccordion = function(targetElem, event, direction) {
+      const controls = targetElem.getAttribute('aria-controls');
+      const controlsElem = document.getElementById(controls);
+      const controlsElemHeight = controlsElem.scrollHeight;
+      const toggled = controlsElem.getAttribute('aria-hidden');
+
+      if (event) {
+        event.preventDefault();
+      }
+
+      function timeoutSet(element) {
+        setTimeout(function() {
+          element.style.height = 'auto';
+        }, 300);
+      }
+
+      if (direction === 'expand' && toggled === 'true') {
+        controlsElem.style.height = `${controlsElemHeight}px`;
+        controlsElem.setAttribute('aria-hidden', 'false');
+        timeoutSet(controlsElem);
+      } else if (direction === 'collapse' && toggled === 'false') {
+        controlsElem.style.height = `${controlsElem.scrollHeight}px`;
+        controlsElem.setAttribute('aria-hidden', 'true');
+        setTimeout(function() {
+          controlsElem.setAttribute('aria-hidden', true);
+          controlsElem.style.height = `${0}px`;
+        }, 50);
+      }
+
+      toggleButton(targetElem, direction);
+    };
+
+    /**
+     * @function toggleAllContent
+     * @param  {String} targetElem The toggle all button
+     * @param  {MouseEvent} event mouse or keyboard
+     * @param  {String} direction The direction of the accordion Expand/Collapse
+     * @return {void}
+     */
+    const toggleAllAccordions = function (targetElem, event, direction) {
+      if (event) {
+        event.preventDefault();
+      }
+
+      // Grab all accordion toggles in the document
+      const buttons = document.querySelectorAll(
+        `.${plugin.settings.toggleBtnClass}`
+      );
+      // Loop through each accordion
+      Array.prototype.forEach.call(buttons, function (button) {
+        toggleAccordion(button, event, direction);
+      });
+
+      // After event toggle aria-expanded attribute
+      targetElem.setAttribute(
+        'aria-expanded',
+        targetElem.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'
+      );
+
+      // After event toggle button text
+      targetElem.textContent =
+        targetElem.textContent === plugin.settings.expandAllOpenText
+          ? (targetElem.textContent = plugin.settings.expandAllCloseText)
+          : (targetElem.textContent = plugin.settings.expandAllOpenText);
+    };
+
+    /**
+     * @function clickHandler
+     * @param  {String} event mouse click
+     * @return {type} {description}
+     */
+    const clickHandler = function(event) {
+      const toggleBtn = event.target.closest(
+        `.${plugin.settings.toggleBtnClass}`
+      );
+      const toggleAll = event.target.closest(
+        `.${plugin.settings.expandAllClass}`
+      );
+
+      const direction =
+        event.target.getAttribute('aria-expanded') === 'true'
+          ? 'collapse'
+          : 'expand';
+
+      if (toggleBtn) {
+        toggleAccordion(toggleBtn, event, direction);
+      }
+      if (toggleAll) {
+        toggleAllAccordions(toggleAll, event, direction);
+      }
+    }
+    /**
+     * @function keyboardHandler
+     * @param  {String} event keyboard press
+     * @return {type} {description}
+     */
+    const keyboardHandler = function (event) {
+      if (event.key == 'Enter' || event.key == ' ') {
+        event.preventDefault()
+        const toggleBtn = event.target.closest(
+          `.${plugin.settings.toggleBtnClass}`
+        );
+        const toggleAll = event.target.closest(
+          `.${plugin.settings.expandAllClass}`
+        );
+      const direction =
+        event.target.getAttribute('aria-expanded') === 'true'
+          ? 'collapse'
+          : 'expand';
+
+      if (toggleBtn) {
+        toggleAccordion(toggleBtn, event, direction);
+      }
+      if (toggleAll) {
+        toggleAllAccordions(toggleAll, event, direction);
+        }
+      }
+    }
+
+    /**
+     * Public variables and methods.
+     * @type {object}
+     */
+    Plugin.prototype = {
+      setup() {
+        const accordions = document.querySelectorAll(plugin.element);
+        let btnId = 0;
+        Array.prototype.forEach.call(accordions, function(accordion) {
+          btnId += 1;
+          accordionToggleSetup(accordion, btnId);
+        });
+
+        if (plugin.settings.expandAllBtn && accordions.length > 1) toggleAllSetup();
+      },
+
+      /**
+       * Setup the accordion toggle button and aria attributes
+       *
+       * @function init
+       * @param {Object} options - User defined options
+       *
+       */
+      init() {
+        // If there are no accordions bail
+        if (!plugin.element) return false;
+
+        // Setup items in the DOM
+        plugin.this.setup();
+
+        // Add event mouse and keyboard listeners
+        document.addEventListener('click', clickHandler, false);
+        document.addEventListener('keydown', keyboardHandler, false)
+      },
+    };
+
+    // Return the plugin
+    return Plugin;
+  }
+);
